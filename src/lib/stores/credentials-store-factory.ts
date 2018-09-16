a9e1d6cc43f6d9fcdc5d3f45c22c624945b07ee9
Michael Bennett <bennett000@users.noreply.github.com>
2018-09-16 13:43:09 -0400

    Runs on my machine now (#10)
    * Fixes typing errors
    
    * Docker now runs on my machine
    
    * Updates README with basic info about the RC config

--- src/lib/stores/credentials-store-factory.ts
+++ src/lib/stores/credentials-store-factory.ts
@@ -5,7 +5,7 @@ import { RedisCredentialsStore } from './RedisCredentialsStore';
 export function credentialsStoreFactory(type: StoreType, dbClients: DBClients, seedData?: any): ICredentialsStore {
   switch (type) {
     case StoreType.memory:
-      return new MemoryCredentialsStore(seedData);
+      return new MemoryCredentialsStore();
     case StoreType.redis:
       return new RedisCredentialsStore(dbClients.redis, 'cred');
   }
